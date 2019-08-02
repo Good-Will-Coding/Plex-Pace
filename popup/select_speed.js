2c21085fad923624e397984839e1b8b6c2da2400
billy <billyliadphillips@gmail.com>
2019-08-02 14:57:50 +0100

    Added in change of background on selected speed

--- popup/select_speed.js
+++ popup/select_speed.js
@@ -2,12 +2,13 @@
   const changeSpeed = chosenRate => {
     const executeSpeedChange = `document.getElementsByTagName('video')[0].playbackRate = ${chosenRate}`;
 
-    let executing = browser.tabs.executeScript({
+    const executing = browser.tabs.executeScript({
       code: executeSpeedChange
     });
 
     const onExecuted = result => {
       console.log(`We executed in all subframes`);
+      changeSelectedSpeedBg(chosenRate);
     };
 
     const onError = error => {
@@ -49,6 +50,23 @@
     }
   };
 
+  const changeSelectedSpeedBg = id => {
+    const chosenSpeedID = document.getElementById(`${id}`);
+    chosenSpeedID.style.background = "black";
+  };
+
   const getSpeed = document.getElementById("popup-content");
-  getSpeed.addEventListener("click", selectSpeed);
+  const setSpeedOnChange = () => {
+    getSpeed.addEventListener("click", selectSpeed);
+  };
+
+  setSpeedOnChange();
 })();
+
+/* TODO
+
+KEY SHORTCUTS
+SELECTED SPEED BACKGROUND - PERSISTANT - FIX MULTIPLE BG CHANGING
+CUSTOM SPEED
+
+*/
