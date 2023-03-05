0107275111d703d1e32d1e72e2e6a0da879a3971
Björn Böing <b.boeing@online.de>
2023-03-05 22:41:45 +0100

    Fix redeclaration of PREVIOUS_SPEED_RATE

--- injections/loadedDataListener.js
+++ injections/loadedDataListener.js
@@ -1,11 +1,13 @@
-const PREVIOUS_SPEED_RATE = 'plex-pace-last-speed-rate';
-const video = document.getElementsByTagName('video')[0];
+(() => {
+  const PREVIOUS_SPEED_RATE = 'plex-pace-last-speed-rate';
+  const video = document.getElementsByTagName('video')[0];
 
-video.addEventListener('loadeddata', async () => {
-  const previousSpeed = await browser.storage.local.get(PREVIOUS_SPEED_RATE);
+  video.addEventListener('loadeddata', async () => {
+    const previousSpeed = await browser.storage.local.get(PREVIOUS_SPEED_RATE);
 
-  if (previousSpeed[PREVIOUS_SPEED_RATE]) {
-    video.playbackRate = previousSpeed[PREVIOUS_SPEED_RATE];
-    console.log('Updated playback speed');
-  }
-});
+    if (previousSpeed[PREVIOUS_SPEED_RATE]) {
+      video.playbackRate = previousSpeed[PREVIOUS_SPEED_RATE];
+      console.log('Updated playback speed');
+    }
+  });
+})();
